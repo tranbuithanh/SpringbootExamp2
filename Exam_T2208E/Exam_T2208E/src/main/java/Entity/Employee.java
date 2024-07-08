49cea6ef30e4aa351ea11013f76f1be82149782f
tranbuithanh <109666928+tranbuithanh@users.noreply.github.com>
2024-07-08 19:51:19 +0700

    Add files via upload

---
+++ Exam_T2208E/Exam_T2208E/src/main/java/Entity/Employee.java
@@ -0,0 +1,52 @@
+package Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double wage;
+
+    // Constructors, getters, and setters
+
+    // Constructor
+    public Employee() {}
+
+    // Constructor with fields
+    public Employee(String name, double wage) {
+        this.name = name;
+        this.wage = wage;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWage() {
+        return wage;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+}
---
+++ Exam_T2208E/Exam_T2208E/src/main/java/Entity/GenerationType.java
@@ -0,0 +1,6 @@
+package Entity;
+
+public class GenerationType {
+    public class IDENTITY {
+    }
+}
